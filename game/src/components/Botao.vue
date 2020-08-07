39b5e26cc8af58bb86d5d2517322bb7cb786be88
Edrei <edreifernandes@gmail.com>
2020-08-06 21:38:24 -0300

    Criando layout da página Home

---
+++ game/src/components/Botao.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button type="button" name="naoClica" id="naoClica" class="btn rounded-circle">
+      <img src="@/assets/ca.jpg" class="rounded-circle" title="não clique" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ game/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container text-center">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item my-5">
+        <h2>Sejá bem vindo aos Clickers Anónimos</h2>
+        <!-- <small>Não clique em nada</small> -->
+      </li>
+
+      <li class="list-group-item">
+        <app-botao></app-botao>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Botao from "@/components/Botao.vue";
+
+export default {
+  components: {
+    "app-botao": Botao,
+  },
+};
+</script>
+
+<style scoped>
+</style>
