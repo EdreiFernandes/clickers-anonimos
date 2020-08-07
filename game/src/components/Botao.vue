c653de1bede2718fe17dcecdd497bac94af9a2da
Edrei <edreifernandes@gmail.com>
2020-08-06 22:12:31 -0300

    Criando sistema básico de botões

--- game/src/components/Botao.vue
+++ game/src/components/Botao.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <button type="button" name="naoClica" id="naoClica" class="btn rounded-circle">
-      <img src="@/assets/ca.jpg" class="rounded-circle" title="não clique" />
+    <button
+      type="button"
+      name="naoClica"
+      id="naoClica"
+      class="btn rounded-circle"
+      @click="clicando()"
+    >
+      <img src="@/assets/ca.jpg" class="rounded-circle" title="Não Clique!" />
     </button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    acrescimo: {
+      type: Number,
+      default: 1,
+    },
+  },
+  methods: {
+    clicando() {
+      this.$emit("ContarClick", this.acrescimo);
+    },
+  },
+};
 </script>
 
 <style scoped>
--- game/src/components/Home.vue
+++ game/src/components/Home.vue
@@ -3,11 +3,11 @@
     <ul class="list-group list-group-flush">
       <li class="list-group-item my-5">
         <h2>Sejá bem vindo aos Clickers Anónimos</h2>
-        <!-- <small>Não clique em nada</small> -->
+        <small v-if="contador > 0">Não clique em ai</small>
       </li>
 
       <li class="list-group-item">
-        <app-botao></app-botao>
+        <app-botao v-on:ContarClick="aumentarContador($event)"></app-botao>
       </li>
     </ul>
   </div>
@@ -19,6 +19,16 @@ import Botao from "@/components/Botao.vue";
 export default {
   components: {
     "app-botao": Botao,
+  },
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    aumentarContador(_add) {
+      this.contador += _add;
+    },
   },
 };
 </script>
